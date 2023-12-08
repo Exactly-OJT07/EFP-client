7fea524e4407175fad57e3864a05301bb56debf7
tvpkhangojt07 <khang.truong-ojt07@devplus.edu.vn>
2023-12-08 18:41:16 +0700

    feat/EFP-9: add email and name search

--- src/hooks/useEmployee.jsx
+++ src/hooks/useEmployee.jsx
@@ -4,7 +4,13 @@ import { QUERY_KEY } from "../constants/query-key";
 
 export const useGetClients = (params) =>
   useQuery(
-    [QUERY_KEY.EMPLOYEE, params.page, params.take, params.search],
+    [
+      QUERY_KEY.EMPLOYEE,
+      params.page,
+      params.take,
+      params.searchByName,
+      params.searchByEmail,
+    ],
     async () => {
       const { data } = await getEmployeeAPI(params);
       return data;
