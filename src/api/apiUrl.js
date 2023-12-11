550a1192413cf5d8a8a28d8513dab4668261970a
NTHung117 <nguyentienhung117@gmail.com>
2023-12-11 10:35:26 +0700

    feat/EFP-6: fix

--- src/api/apiUrl.js
+++ src/api/apiUrl.js
@@ -4,4 +4,7 @@ import { API_URL } from "../constants/constants";
 export const getEmployeeAPI = (params) =>
   axios.get(API_URL.EMPLOYEE, { params });
 
+export const deleteEmployeeAPI = (params) =>
+  axios.delete(API_URL.EMPLOYEE, { params });
+
 export const getProjectApi = (params) => axios.get(API_URL.PROJECT, { params });
