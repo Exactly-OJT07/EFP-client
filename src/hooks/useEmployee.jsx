8bb4d72b484a69a5011a37e66c2ed0c20e38b69c
Sondtgcd191140 <149054180+Sondtgcd191140@users.noreply.github.com>
2023-12-13 16:05:28 +0700

    feat/EFP-11: addemployee

--- src/hooks/useEmployee.jsx
+++ src/hooks/useEmployee.jsx
@@ -2,7 +2,6 @@ import { useQuery, useQueryClient, useMutation } from "@tanstack/react-query";
 import { getEmployeeAPI, getEmployeeDetailApi, createEmployeeAPI } from "../api/apiUrl";
 import { QUERY_KEY } from "../constants/query-key";
 import { useQueryClient, useMutation } from "@tanstack/react-query";
-import { checkDuplicateEmployeeAPI } from "../api/apiUrl";
 
 export const useGetClients = (params) =>
   useQuery(
@@ -24,27 +23,13 @@ export const useCreateEmployee = () => {
   const mutation = useMutation(
     (newEmployee) => createEmployeeAPI(newEmployee),
     {
-      onSuccess: () => {
+      onSuccess: (data) => {
         queryClient.invalidateQueries([QUERY_KEY.EMPLOYEE]);
       },
     },
   );
 
-  const handleCreateEmployee = async (newEmployee) => {
-    const isDuplicate = await checkDuplicateEmployeeAPI(
-      "fieldName",
-      newEmployee.fieldName,
-    );
-    console.log("Is Duplicate:", isDuplicate);
-
-    if (!isDuplicate) {
-      mutation.mutate(newEmployee);
-    } else {
-      console.log("Duplicate found. Not creating employee.");
-    }
-  };
-
-  return { ...mutation, handleCreateEmployee };
+  return mutation;
 };
 
 export const useGetOneEmployee = (id) => {
--- src/pages/employees/ManageEmployee.jsx
+++ src/pages/employees/ManageEmployee.jsx
@@ -1,95 +1,30 @@
 import React, { useState } from "react";
-import { Button, Form, DatePicker, Modal } from "antd";
+import { Button } from "antd";
+import { PlusOutlined } from "@ant-design/icons";
 import "../../styles/ManageEmployee.css";
 import ReadEmployee from "./components/ReadEmployee";
 import CreateEmployee from "./components/CreateEmployee";
-import { useCreateEmployee } from "../../hooks/useEmployee";
-import { Cloudinary } from "@cloudinary/url-gen";
-import handleSubmit from "./components/CreateEmployee";
 
 function ManageEmployee() {
-  const [confirmLoading, setConfirmLoading] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const { RangePicker } = DatePicker;
-  const createEmployeeMutation = useCreateEmployee();
-  const [employeeData, setEmployeeData] = useState([]);
 
-  const [newCode, setNewCode] = useState("");
-
-  const [newName, setNewName] = useState("");
-  const [newPhone, setNewPhone] = useState("");
-  const [newDob, setNewDob] = useState("");
-  const [newCid, setNewCid] = useState("");
-  const [newGender, setNewGender] = useState("");
-  const [newPosition, setNewPosition] = useState("");
-
-  const [newSkill, setNewSkill] = useState("");
-  const [newExperience, setNewExperience] = useState("");
-  const [imageUrl, setImageUrl] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const cld = new Cloudinary({ cloud: { cloudName: "dvm8fnczy" } });
-  const [newDescription, setNewDescription] = useState([]);
-
-  const [newIsManager, setNewIsManager] = useState(false);
-  const [newLineManager, setNewLineManager] = useState(null);
-
-  const [newExp, setNewExp] = useState("");
-  const newExperienceObject = { skill: newSkill, exp: newExp };
-  const [newEmail, setNewEmail] = useState("");
-  const [newJoinDate, setNewJoinDate] = useState("");
-  const [newFireDate, setNewFireDate] = useState("");
-  const [newAvatar, setNewAvatar] = useState("");
-
-  const [viewModalOpen, setViewModalOpen] = useState(false);
-
-  const handleOpenOk = async () => {
-    try {
-      console.log("Create OK");
-      setConfirmLoading(true);
-      try {
-        handleSubmit();
-        console.log("Create OK");
-      } catch (error) {
-        console.log(error);
-      }
-
-      setTimeout(() => {
-        setIsModalOpen(false);
-        setConfirmLoading(false);
-      }, 2000);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const showModal = () => {
-    setIsModalOpen(true);
-  };
-
-  const handleCancel = () => {
-    console.log("Create Cancel");
+  const handleCloseModal = () => {
     setIsModalOpen(false);
   };
 
-  const [formView] = Form.useForm();
-
   return (
     <>
-      <Button type="primary" onClick={showModal}>
-        Add Employee
-      </Button>
-
-      <Modal
-        title="Add Employee"
-        open={isModalOpen}
-        onOk={handleOpenOk}
-        confirmLoading={confirmLoading}
-        onCancel={handleCancel}
-        width={1000}
-        height={1000}
-      >
-        <CreateEmployee />
-      </Modal>
+      <div>
+        <Button type="primary" onClick={() => setIsModalOpen(true)}>
+          <PlusOutlined /> New Employee
+        </Button>
+        <CreateEmployee
+          isModalOpen={isModalOpen}
+          setIsModalOpen={setIsModalOpen}
+          width="1000px"
+          onCancel={handleCloseModal}
+        />
+      </div>
 
       <ReadEmployee />
     </>
